5136d8d1d6f70ecbd122fe29de2f64402d15910f
Andey Dvornik <ggjxnf@gmail.com>
2015-01-14 00:33:12 +0300

    audioPlayer and play slow

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+        } else {
+            println("file not found")
+        }
+        
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +33,11 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSlowAudio(sender: AnyObject) {
+        // var url = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3")
+        // audioPlayer = AVAudioPlayer(contentsOfURL: url, error: nil)
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
     }
 
     /*
