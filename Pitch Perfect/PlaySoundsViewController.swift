8fc0e529f2a2bd32a8bed6bd4f6463a7dfbd001d
Andrey Dvornik <ggjxnf@gmail.com>
2015-01-14 17:54:32 +0300

    playChipmunkAudio

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -12,6 +12,8 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
+    var audioEngine: AVAudioEngine!
+    var audioFile:AVAudioFile!
     var receivedAudio: RecordedAudio!
     
     override func viewDidLoad() {
@@ -25,6 +27,8 @@ class PlaySoundsViewController: UIViewController {
         }*/
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
         
         // Do any additional setup after loading the view.
     }
@@ -46,6 +50,31 @@ class PlaySoundsViewController: UIViewController {
         playAudio()
     }
     
+    @IBAction func playChipmunkAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
     func playAudio() {
         audioPlayer.stop()
         audioPlayer.currentTime = 0
@@ -54,6 +83,7 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func stopPlayingAudio(sender: AnyObject) {
         audioPlayer.stop()
+        audioEngine.stop()
     }
     
     /*
